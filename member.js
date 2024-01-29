58b87ecf5b1e6534c2ba05e44ab45a80c9afd032
Emily Nelson <63617692+emilymnelson@users.noreply.github.com>
2024-01-29 20:34:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,30 @@
+function skillsMember() {
+    var path = document.location.pathname;
+    if (path.indexOf('member') > -1) {
+        var member = document.querySelector('.member');
+        var skills = member.querySelector('.skills');
+        var skillsList = skills.querySelectorAll('.skills__list li');
+        var skillsValue = skills.querySelectorAll('.skills__value');
+        var skillsValueList = skills.querySelectorAll('.skills__value li');
+        var skillsValueListWidth = [];
+        var skillsValueListWidthMax = 0;
+        var skillsValueListWidthMaxIndex = 0;
+        var skillsValueListWidthMaxValue = 0;
+        var skillsValueListWidthMaxValueIndex = 0;
+        var skillsValueListWidthMaxValueValue = 0;
+        var skillsValueListWidthMaxValueValueIndex = 0;
+        var skillsValueListWidthMaxValueValueValue = 0;
+        var skillsValueListWidthMaxValueValueValueIndex = 0;
+        var skillsValueListWidthMaxValueValueValueValue = 0;
+        var skillsValueListWidthMaxValueValueValueValueIndex = 0;
+        var skillsValueListWidthMaxValueValueValueValueValue = 0;
+        var skillsValueListWidthMaxValueValueValueValueValueIndex = 0;
+        var skillsValueListWidthMaxValueValueValueValueValueValue = 0;
+        var skillsValueListWidthMaxValueValueValueValueValueValueIndex = 0;
+        var skillsValueListWidthMaxValueValueValueValueValueValueValue = 0;
+        var skillsValueListWidthMaxValueValueValueValueValueValueValueIndex = 0;
+        var skillsValueListWidthMaxValueValueValueValueValueValueValueValue = 0;
+        var skillsValueListWidthMaxValueValueValueValueValueValueValueValueIndex = 0;
+        var skillsValueListWidthMaxValueValueValueValueValueValueValueValueValue = 0;
+        var skillsValueListWidthMaxValueValueValueValueValueValueValueValueValueIndex = 0;
+        var skillsValueListWidthM
